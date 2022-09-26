7eb403e276d3e86afe0c09f910fd73c1ddd59c96
lucian-coniglio <89875842+lucian-coniglio@users.noreply.github.com>
2022-09-26 19:04:59 -0300

    Add files via upload

---
+++ Parcial 1 (Lucián Coniglio)/js_contacto.js
@@ -0,0 +1,25 @@
+function subir() {alert("Gracias por compartir tu opinión!")}
+function temaClaro() {
+    document.body.style.backgroundColor = "rgb(240,240,240)";
+    document.getElementById("header").style.backgroundColor = "lightgrey";
+    document.getElementById("footer").style.backgroundColor = "lightgrey";
+    document.getElementById("header").style.color = "black";
+    document.getElementById("footer").style.color = "black";
+    document.getElementById("articulo").style.color = "black";
+    document.getElementById("lista_redes").style.color = "black";
+    document.getElementById("lista_contactos").style.color = "black";
+    document.getElementById("tabla_opciones").style.color = "black";
+    document.getElementById("tabla_opciones").style.backgroundColor = "rgb(240,240,240)";
+  }
+function temaOscuro() {
+    document.body.style.backgroundColor = "rgb(40,40,40)";
+    document.getElementById("header").style.backgroundColor = "rgb(80,80,80)";
+    document.getElementById("footer").style.backgroundColor = "rgb(80,80,80)";
+    document.getElementById("header").style.color = "white";
+    document.getElementById("footer").style.color = "white";
+    document.getElementById("articulo").style.color = "white";
+    document.getElementById("lista_redes").style.color = "white";
+    document.getElementById("lista_contactos").style.color = "white";
+    document.getElementById("tabla_opciones").style.color = "white";
+    document.getElementById("tabla_opciones").style.backgroundColor = "grey";
+  }
---
+++ Parcial 1 (Lucián Coniglio)/js_platos.js
@@ -0,0 +1,24 @@
+function temaClaro() {
+  document.body.style.backgroundImage = "url('https://previews.123rf.com/images/katrink03/katrink031709/katrink03170900012/85572829-patr%C3%B3n-transparente-de-vector-de-hamburguesas-de-contorno-negro-fondo-para-banner-embalaje-men%C3%BA-.jpg')";
+    document.getElementById("header").style.backgroundColor = "lightgrey";
+    document.getElementById("footer").style.backgroundColor = "lightgrey";
+    document.getElementById("header").style.color = "black";
+    document.getElementById("footer").style.color = "black";
+    document.getElementById("articulo").style.color = "black";
+    document.getElementById("lista_redes").style.color = "black";
+    document.getElementById("lista_contactos").style.color = "black";
+    document.getElementById("tabla_opciones").style.color = "black";
+    document.getElementById("tabla_opciones").style.backgroundColor = "rgb(240,240,240)";
+  }
+function temaOscuro() {
+  document.body.style.backgroundImage = "url('https://i.pinimg.com/736x/99/67/9a/99679ad578fac6b0bb9925b758472973.jpg')";
+    document.getElementById("header").style.backgroundColor = "rgb(80,80,80)";
+    document.getElementById("footer").style.backgroundColor = "rgb(80,80,80)";
+    document.getElementById("header").style.color = "white";
+    document.getElementById("footer").style.color = "white";
+    document.getElementById("articulo").style.color = "white";
+    document.getElementById("lista_redes").style.color = "white";
+    document.getElementById("lista_contactos").style.color = "white";
+    document.getElementById("tabla_opciones").style.color = "white";
+    document.getElementById("tabla_opciones").style.backgroundColor = "grey";
+  }
